877520429bb7623e3b7711c242e313ce45277aff
Wilhelm <47418895@continental.edu.pe>
2020-12-31 10:42:30 -0500

    Modelo de cliente.

--- app/src/main/java/com/codec/marketfriendapp/Models/Response/ClienteResponse.java
+++ app/src/main/java/com/codec/marketfriendapp/Models/Response/ClienteResponse.java
@@ -1,4 +1,45 @@
 package com.codec.marketfriendapp.Models.Response;
-
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 public class ClienteResponse {
+    @SerializedName("codigo")
+    @Expose
+    private String codigo;
+    @SerializedName("datos")
+    @Expose
+    private String datos;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public ClienteResponse() {
+    }
+
+    /**
+     *
+     * @param codigo
+     * @param datos
+     */
+    public ClienteResponse(String codigo, String datos) {
+        super();
+        this.codigo = codigo;
+        this.datos = datos;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDatos() {
+        return datos;
+    }
+
+    public void setDatos(String datos) {
+        this.datos = datos;
+    }
 }
